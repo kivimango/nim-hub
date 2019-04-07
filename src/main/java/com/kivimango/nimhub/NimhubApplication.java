32fbccc146d8f5f1d5824766c17066550e4ea558
kivimango <balazsviktor23@gmail.com>
2019-04-07 12:25:00 +0200

    Initial commit

---
+++ src/main/java/com/kivimango/nimhub/NimhubApplication.java
@@ -0,0 +1,19 @@
+package com.kivimango.nimhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kivimango
+ * @since 0.1
+ * @version 0.1
+ */
+
+@SpringBootApplication
+public class NimhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kivimango/nimhub/NimhubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kivimango.nimhub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NimhubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
