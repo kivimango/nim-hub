cbaf0d9b4d9da0f581313a55da07c4637a0e5a81
kivimango <balazsviktor23@gmail.com>
2019-04-11 15:55:17 +0200

    Added regexp for username and @Length changed to @Size

--- src/main/java/com/kivimango/nimhub/rest/UserRegisterForm.java
+++ src/main/java/com/kivimango/nimhub/rest/UserRegisterForm.java
@@ -3,19 +3,22 @@ package com.kivimango.nimhub.rest;
 import org.hibernate.validator.constraints.Length;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 final class UserRegisterForm {
 
     @NotNull(message = "You must supply a username")
-    @Length(min = 3, max = 100, message = "The username must be at least {min} and max {max} characters long")
+    @Size(min = 3, max = 100, message = "The username must be at least {min} and max {max} characters long")
+    @Pattern(regexp = "^[a-z0-9_]+$", message = "The username should contain only alphanumeric characters and underscores")
     private String username;
 
     @NotNull(message = "You must supply a password")
-    @Length(min = 3, max = 255, message = "The password must be at least {min} and max {max} characters long")
+    @Size(min = 3, max = 255, message = "The password must be at least {min} and max {max} characters long")
     private String password;
 
     @NotNull(message = "You must supply an email address")
-    @Length(min = 3, max = 255, message = "The email must be at least {min} and max {max} characters long")
+    @Size(min = 3, max = 255, message = "The email must be at least {min} and max {max} characters long")
     @Email(message = "The email address must be a valid RFC822 email address")
     private String email;
 
