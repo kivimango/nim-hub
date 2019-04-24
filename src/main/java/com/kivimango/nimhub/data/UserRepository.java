4fbc98eb8e03d27f3afbda719fe495e2e3c20391
kivimango <balazsviktor23@gmail.com>
2019-04-24 18:59:53 +0200

    Added findByUsername()

--- src/main/java/com/kivimango/nimhub/data/UserRepository.java
+++ src/main/java/com/kivimango/nimhub/data/UserRepository.java
@@ -3,8 +3,11 @@ package com.kivimango.nimhub.data;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String name);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 }
