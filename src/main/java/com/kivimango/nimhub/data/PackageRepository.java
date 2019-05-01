9d50ffa6626c22f0c6bbec5725be878ae0d86fac
kivimango <balazsviktor23@gmail.com>
2019-05-01 23:44:29 +0200

    Added findByNameaAndVersion()

--- src/main/java/com/kivimango/nimhub/data/PackageRepository.java
+++ src/main/java/com/kivimango/nimhub/data/PackageRepository.java
@@ -3,6 +3,9 @@ package com.kivimango.nimhub.data;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PackageRepository extends CrudRepository<Package, Long> {
+    Optional<Package> findByNameAndVersion(String name, String version);
 }
