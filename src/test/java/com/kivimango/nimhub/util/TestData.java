92d4fd2ec7689e1620a6ffe2586650c204b9f49b
kivimango <balazsviktor23@gmail.com>
2019-05-07 18:53:32 +0200

    Added test user data

--- src/test/java/com/kivimango/nimhub/util/TestData.java
+++ src/test/java/com/kivimango/nimhub/util/TestData.java
@@ -1,8 +1,6 @@
 package com.kivimango.nimhub.util;
 
-import com.kivimango.nimhub.data.AuthorDto;
-import com.kivimango.nimhub.data.PackageDto;
-import com.kivimango.nimhub.data.TagDto;
+import com.kivimango.nimhub.data.*;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -15,11 +13,15 @@ public class TestData {
     public static final String web = "https://github.com/kivimango/nim-hub";
     public static final String version = "1.0-STABLE";
 
+    public static final Long userId = 1L;
+    public static final String username = "testUser";
+    public static final String email = "test@test.com";
+
     public static Set<TagDto> getTestTagDtos() {
         Set<TagDto> tagDto = new HashSet<>();
         tagDto.add(TagDto.of(tag));
         return tagDto;
     }
-
     public static PackageDto dto = PackageDto.of(1L, AuthorDto.of(1L, "testUser", "test@testemail.com"), packageName, description, license, web, version, getTestTagDtos(), false);
+
 }
